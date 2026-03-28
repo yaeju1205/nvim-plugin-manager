e5ab13145e57e6324521dad9663944ff9602f616
yaeju1205 <gimleni593@gmail.com>
2026-03-28 11:54:43 +0900

    fix: Remove plugin directory

--- lua/plugin-manager.lua
+++ lua/plugin-manager.lua
@@ -20,7 +20,7 @@ plugin_manager.git_prefix = "https://" .. plugin_manager.git_host .. "/"
 --- Added by nvim-plugin-manager for plugin infos
 --- can control to plugins info and namespaces
 --- @alias vim.plugins PluginManager
-vim.plugin = {}
+vim.plugins = {}
 
 --- @type table<string, PluginManager.PluginNamespace>
 local plugin_namespaces = {}
--- plugin/plugin-manager.lua
+++
@@ -1,4 +0,0 @@
---- Added by nvim-plugin-manager for plugin infos
---- can control to plugins info and namespaces
---- @alias vim.plugins PluginManager
-vim.plugin = require("plugin-manager")
